8cdaf0cd0d0534d692461f3ee8044954c8a6e426
zheng <765324639@qq.com>
2017-12-15 13:25:22 +0800

    提交前添加提示确认部分

--- src/main/webapp/js/index.js
+++ src/main/webapp/js/index.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
     initWebpath();
-    initSubmitBtn();
 })
 
 var webpath;
@@ -12,31 +11,50 @@ function initWebpath() {
     }
 }
 
-function initSubmitBtn() {
-    $("#submit").on('click', function() {
-        var params = [];
-        $(".english-data").each(function() {
-            var param = {
-                "id": $(this).data("id"),
-                "english": $(this).val(),
-            }
-            params.push(param)
-        })
-        console.log(JSON.stringify(params))
-        $.ajax({
-            url: 'http://localhost/upload-answer',
-            type: 'POST',
-            dataType: 'json',
-            data: JSON.stringify(params),
-            headers: {'Content-Type': 'application/json'},
-            success: function(res) {
-                if (res.code == 0) {
-                    layer.msg("提交成功");
-                    window.location.href = webpath + 'show-result';
-                } else {
-                    alert('提交失败\n' + res.msg);
-                }
-            }
-        })
+function checkData() {
+    var data = getData();
+    checkCompleteInfoData(data);
+}
+
+function getData() {
+    var params = [];
+    $(".english-data").each(function() {
+        var param = {
+            "id": $(this).data("id"),
+            "english": $.trim($(this).val()),
+        }
+        params.push(param)
     })
+    return params;
+}
+
+function submitResult() {
+    var data = getData();
+    $.ajax({
+        url: webpath + 'upload-answer',
+        type: 'POST',
+        dataType: 'json',
+        data: JSON.stringify(data),
+        headers: {'Content-Type': 'application/json'},
+        success: function(res) {
+            if (res.code == 0) {
+                layer.msg("提交成功");
+                window.location.href = webpath + 'show-result';
+            } else {
+                alert('提交失败\n' + res.msg);
+            }
+        }
+    })
+}
+
+function checkCompleteInfoData(datas) {
+    var unCompleteNum = 0;
+    for (var i = 0; i < datas.length; i++) {
+        if (!datas[i].english) {
+            unCompleteNum++;
+        }
+    }
+    $("#confirmSubmitInfo").text('还有' + unCompleteNum + '道题没有完成，是否确定提交?');
+    $("#myModal").modal();
+    return false;
 }
