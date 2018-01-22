987baca2b2cf8f779ba55be12705d343cee3ed78
zheng <765324639@qq.com>
2018-01-22 15:00:07 +0800

    登录和注册部分添加校验

--- src/main/webapp/static/js/user/registry.js
+++ src/main/webapp/static/js/user/registry.js
@@ -1,28 +1,26 @@
 function registry() {
-    if (!checkData()) {
-        alertErrorMsg('请检查数据是否填写完整');
-        return false;
-    }
-    var ajaxParam = getAjaxJsonParamObject('/user/register', 'POST', getData())
-    ajaxParam.success = function(data) {
-        console.log(JSON.stringify(data));
-        if (isAjaxSuccess(data)) {
-            location.href = '/user/login';
-        } else {
-            logAndAlertErrorMsg(data, '注册失败,' + data.msg);
+    $("#registryForm").validate({
+        debug: true,
+        onkeyup: function(element, event) {
+            //去除左侧空格
+            var value = this.elementValue(element).replace(/^\s+/g, "");
+            $(element).val(value);
+        },
+        submitHandler: function () {
+            // 校验通过
+            var ajaxParam = getAjaxJsonParamObject('/user/register', 'POST', getData());
+            ajaxParam.success = function (data) {
+                console.log(JSON.stringify(data));
+                if (isAjaxSuccess(data)) {
+                    location.href = '/user/login';
+                } else {
+                    logAndAlertErrorMsg(data, '注册失败,' + data.msg);
+                }
+            };
+            $.ajax(ajaxParam);
         }
-    };
-    $.ajax(ajaxParam);
-}
-
-function checkData() {
-    var data = getData();
-    for (var key in data) {
-        if (!data[key]) {
-            return false;
-        }
-    }
-    return true;
+    });
+    return false;
 }
 
 function getData() {
